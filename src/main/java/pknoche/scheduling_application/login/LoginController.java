dca0eeb262001d4f0b2cc074c03525f5113bf7aa
Philipp Knoche <knoche.philipp@gmail.com>
2023-04-07 14:05:15 -0500

    Initial Commit

---
+++ src/main/java/pknoche/scheduling_application/login/LoginController.java
@@ -0,0 +1,25 @@
+package pknoche.scheduling_application.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void onLoginButtonClick(ActionEvent actionEvent) {
+    }
+}
