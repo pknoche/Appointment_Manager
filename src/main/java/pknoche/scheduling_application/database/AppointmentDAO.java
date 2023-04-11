8ca8304dd0b3290e8f80b94b136bd5c8bcd8605b
Philipp Knoche <130101659+pknoche@users.noreply.github.com>
2023-04-11 18:03:37 -0500

    Created additional GUI elements and implemented additional data access methods

--- src/main/java/pknoche/scheduling_application/database/AppointmentDAO.java
+++ src/main/java/pknoche/scheduling_application/database/AppointmentDAO.java
@@ -9,41 +9,67 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class AppointmentDAO {
-    private static ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
+        public static void create(Appointment appointment) {
+        try {
+            String sql = "INSERT INTO client_schedule.appointments (Title, Description, Location, Type, Start, " +
+                    "End, Create_Date, Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID, " +
+                    "Contact_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    public static void Create(Appointment appointment) {
+
+            PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(sql);
+            ps.setString(1, appointment.getTitle());
+            ps.setString(2, appointment.getDescription());
+            ps.setString(3, appointment.getLocation());
+            ps.setString(4, appointment.getType());
+            ps.setTimestamp(5, Timestamp.valueOf(appointment.getStart()));
+            ps.setTimestamp(6, Timestamp.valueOf(appointment.getEnd()));
+            ps.setTimestamp(7, Timestamp.valueOf(appointment.getCreate_Date()));
+            ps.setString(8, appointment.getCreated_By());
+            ps.setTimestamp(9, Timestamp.valueOf(appointment.getLast_Update()));
+            ps.setString(10, appointment.getLast_Updated_By());
+            ps.setInt(11, appointment.getCustomer_ID());
+            ps.setInt(12, appointment.getUser_ID());
+            ps.setInt(13, appointment.getContact_ID());
+            System.out.println(ps);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error creating new appointment");
+            System.out.println(e.getMessage());
+        }
 
     }
 
-    public static void Read(String appointmentId) {
+    public static void read(String appointmentId) {
 
     }
 
-    public static void Update(String appointmentId) {
+    public static void update(String appointmentId) {
     }
 
-    public static void Delete(String appointmentId) {
+    public static void delete(String appointmentId) {
     }
 
     public static ObservableList<Appointment> getAll() {
+        ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
         try {
-        String sql = "SELECT * FROM client_schedule.appointments";
-        PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(sql);
-        ResultSet rs = ps.executeQuery();
+            String sql = "SELECT * FROM client_schedule.appointments";
+            PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
         while(rs.next()) {
             int Appointment_ID = rs.getInt("Appointment_ID");
             String Title = rs.getString("Title");
             String Description = rs.getString("Description");
             String Location = rs.getString("Location");
             String Type = rs.getString("Type");
-            Timestamp Start = rs.getTimestamp("Start");
-            Timestamp End = rs.getTimestamp("End");
-            Timestamp Create_Date = rs.getTimestamp("Create_Date");
+            LocalDateTime Start = rs.getTimestamp("Start").toLocalDateTime();
+            LocalDateTime End = rs.getTimestamp("End").toLocalDateTime();
+            LocalDateTime Create_Date = rs.getTimestamp("Create_Date").toLocalDateTime();
             String Created_By = rs.getString("Created_By");
-            Timestamp Last_Update = rs.getTimestamp("Last_Update");
+            LocalDateTime Last_Update = rs.getTimestamp("Last_Update").toLocalDateTime();
             String Last_Updated_By = rs.getString("Last_Updated_By");
             int Customer_ID = rs.getInt("Customer_ID");
             int User_ID = rs.getInt("User_ID");
--- src/main/java/pknoche/scheduling_application/database/CustomerDAO.java
+++ src/main/java/pknoche/scheduling_application/database/CustomerDAO.java
@@ -9,25 +9,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class CustomerDAO {
-    private static ObservableList<Customer> allCustomers = FXCollections.observableArrayList();
-
-    public static void Create(Customer customer) {
+    public static void create(Customer customer) {
 
     }
 
-    public static void Read(String customerId) {
+    public static void read(String customerId) {
 
     }
 
-    public static void Update(String customerId) {
+    public static void update(String customerId) {
     }
 
-    public static void Delete(String customerId) {
+    public static void delete(String customerId) {
     }
 
     public static ObservableList<Customer> getAll() {
+        ObservableList<Customer> allCustomers = FXCollections.observableArrayList();
         try {
             String sql = "SELECT * FROM client_schedule.customers";
             PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(sql);
@@ -38,9 +38,9 @@ public class CustomerDAO {
                 String address = rs.getString("Address");
                 String postal_Code = rs.getString("Postal_Code");
                 String phone = rs.getString("Phone");
-                Timestamp create_Date = rs.getTimestamp("Create_Date");
+                LocalDateTime create_Date = rs.getTimestamp("Create_Date").toLocalDateTime();
                 String created_By = rs.getString("Created_By");
-                Timestamp last_Update = rs.getTimestamp("Last_Update");
+                LocalDateTime last_Update = rs.getTimestamp("Last_Update").toLocalDateTime();
                 String last_Updated_By = rs.getString("Last_Updated_By");
                 int division_ID = rs.getInt("Division_ID");
                 Customer customer = new Customer(customer_ID, customer_Name, address, postal_Code, phone, create_Date, created_By, last_Update, last_Updated_By, division_ID);
